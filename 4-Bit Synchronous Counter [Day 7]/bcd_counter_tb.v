7daeada4430c312b5964e4364eaf890bef95081d
Padmayyagari Mayavathi <154953287+PadmayyagariMayavathi@users.noreply.github.com>
2024-01-03 01:07:04 +0530

    Create bcd_counter_tb.v

---
+++ 4-Bit Synchronous Counter [Day 7]/bcd_counter_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+module bcd_counter_tb;
+
+	// Inputs
+	reg [0:0] clock;
+	reg clear;
+
+	// Outputs
+	wire [3:0] count;
+
+	// Instantiate the Unit Under Test (UUT)
+	bcd_counter uut (
+		.clock(clock), 
+		.count(count), 
+		.clear(clear)
+	);
+
+	initial begin
+		// Initialize Inputs
+		clock = 0;
+		clear = 1;
+#25 clear=0;
+#250 clear=1;
+#25 clear=0;
+#450 $finish;
+
+		end
+	always 
+	#10 clock=~clock;
+      
+endmodule
